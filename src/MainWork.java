ecaa06f85c5d7e82b08ef5945ffa5b7ce29ef2a1
Aleksey <stalk.gladiator09@gmail.com>
2022-12-05 05:52:03 +0500

    first commit

---
+++ src/MainWork.java
@@ -0,0 +1,39 @@
+import java.sql.*; // программа для ведения учета
+import javax.swing.*;
+// import java.swing.*;
+
+public class MainWork {
+
+    /**
+     * Объект подключения к базе данных
+     */
+    static Connection conn = null;
+
+    /**
+     * Объект для создания SQL-запросов
+     */
+    static Statement stmt = null;
+
+    /**
+     * Основное окно программы
+     */
+    static JFrame mainFrame = null;
+
+    /**
+     * Дополнительное окно
+     */
+    static JFrame tempFrame = null;
+
+    /**
+     * Объект полоса прокрутки
+     */
+    static JScrollPane scrollPane;
+
+    public static void main(String[] args) {
+        mainFrame = new JFrame("mainFrame");
+    }
+
+    protected static JTable toTableView(ResultSet rs) {
+        return null;
+    }
+}
---
+++ src/workWith.sql
@@ -0,0 +1,23 @@
+-- Active: 1669031617602@@127.0.0.1@3306@course
+INSERT INTO guest (fullname,birth,hostel,course,classGroup,trainer_id)
+VALUES
+  ("Victoria Reeves","2022-06-21 07","Eget Varius Associates",2,"8725",83),
+  ("Quemby Fowler","2022-05-04 22","Id Risus Corporation",3,"6302",94),
+  ("Graiden Dorsey","2023-06-16 04","Feugiat Tellus Incorporated",1,"0533",99),
+  ("Yvette Holder","2023-01-17 09","Nunc PC",3,"1415",89),
+  ("Isabella Hopkins","2021-12-07 13","Sed Dui Fusce LLC",4,"5764",87),
+  ("Alika Lott","2023-05-01 10","Neque Consulting",3,"5725",100),
+  ("Zachary Fulton","2023-11-18 09","Aliquam Auctor Limited",2,"5099",71),
+  ("Willa Phelps","2023-07-10 22","Nec Industries",3,"4152",80),
+  ("Hop Buckner","2022-09-12 14","Sed Dictum LLP",2,"2548",78),
+  ("Kasimir Spence","2021-12-28 02","Dapibus Gravida Corp.",2,"6937",85),
+  ("Camilla Parrish","2023-01-30 19","Duis Volutpat Nunc Corporation",3,"4875",77),
+  ("Rowan Banks","2022-04-30 00","Euismod Ac Industries",2,"3214",65),
+  ("Brooke Frank","2022-06-27 01","Vestibulum Ante Incorporated",1,"2519",67),
+  ("Wayne Miranda","2022-04-15 03","Magna Tellus Limited",2,"9331",78),
+  ("Autumn Herrera","2023-06-27 13","Vestibulum Inc.",3,"2540",65),
+  ("Lionel Joyce","2022-08-10 10","Vitae Erat Consulting",1,"6326",63),
+  ("Kane Sexton","2022-07-13 00","Tellus Non Corporation",2,"2180",65),
+  ("Jessamine Berry","2023-10-30 13","Sagittis Associates",1,"2066",77),
+  ("Sawyer Wynn","2022-11-15 18","Ornare Placerat Orci Limited",2,"1212",88),
+  ("Byron Diaz","2023-05-22 02","Quam Elementum At Ltd",1,"4751",85);
