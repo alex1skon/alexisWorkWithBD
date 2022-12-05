48f36a32247e215aa1a95509e0ed60e90bbe3848
Aleksey <stalk.gladiator09@gmail.com>
2022-12-05 23:53:49 +0500

    Добавление fromDB

--- src/MainWork.java
+++ src/MainWork.java
@@ -1,7 +1,8 @@
 import java.sql.*; // программа для ведения учета
 import javax.swing.*;
 import java.awt.*;
-// import java.swing.*;
+import java.awt.Event.*;
+import java.util.logging.*;
 
 public class MainWork {
 
@@ -40,7 +41,35 @@ public class MainWork {
     }
 
     private Component setMenu() {
-        
+        Box mainMenu = new Box(BoxLayout.X_AXIS);
+        JButton guestButton = new JButton("Гости");
+        guestButton.addActionListener(e -> {
+            ResultSet rs = null;
+            System.out.println("Guests ! :");
+
+        });
+
         return null;
+    }
+
+    private static ResultSet fromDB(String sqlString) {
+        ResultSet localRS = null;
+        conn = null;
+        stmt = null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            try {
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/course", "forCourse", "1234");
+                stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                localRS = stmt.executeQuery(sqlString);
+            } catch (SQLException SqlEx) {
+                Logger.getLogger(MainWork.class.getName()).log(Level.SEVERE, null, SqlEx);
+                System.out.println("Проблема с запосом в БД!");
+            }
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(MainWork.class.getName()).log(Level.SEVERE, null, e));
+            System.out.println("Проблема с подключением!");
+        }
+        return localRS;
     }
 }
